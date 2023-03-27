09a27181b483001fe1688d3b5318538a537ede42
aseelflihan <aseelflihan@gmail.com>
2023-03-27 20:49:43 +0300

    feat(hero section): add background color

--- src/components/Herro/index.js
+++ src/components/Herro/index.js
@@ -4,16 +4,22 @@ import DonateButton from "./DonateButton";
 import IntroductionImage from "./IntroductionImage";
 import IntroductionText from "./IntroductionText";
 
+const HeroContainer = ({ children }) => {
+    return <div style={{ backgroundColor: "#f1f1f1" }}>{children}</div>;
+};
+
 const Hero = () => {
     return (
-        <div className={styles.container}>
-            <div className={styles["inner-container"]}>
-                <IntroductionText className={styles.introductionText} />
-                <DonateButton className={styles.button} />
+        <HeroContainer>
+            <div className={styles.container}>
+                <div className={styles["inner-container"]}>
+                    <IntroductionText />
+                    <DonateButton />
+                </div>
+                <IntroductionImage />
+                <hr className={styles.line} />
             </div>
-            <IntroductionImage className={styles.image} />
-            <hr className={styles.line} />
-        </div>
+        </HeroContainer>
     );
 };
 
