ac17460c1257b565d7049972e9276313b6738481
Sarah Salem <55980263+SarahSalem-hub@users.noreply.github.com>
2023-03-27 16:40:11 +0300

    feat(causes): add the syled file with it's styled-components

---
+++ src/components/Causes/causes.styled.jsx
@@ -0,0 +1,46 @@
+import styled from "@emotion/styled";
+
+export const CausesContainer = styled.div`
+    width: 100%;
+    background-color: #f1f1f1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    color: #3c4347;
+`;
+export const CausesWord = styled.h1`
+    font-style: normal;
+    font-weight: 700;
+    font-size: 56px;
+    line-height: 84px;
+    top: 41px;
+`;
+
+export const ThreeCausesDiv = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    flex-flow: wrap;
+`;
+
+export const SingleCause = styled.div`
+    width: 400px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 10px;
+`;
+export const Title = styled.p`
+    text-align: center;
+    font-size: 32px;
+    line-height: 48px;
+    font-weight: 700;
+`;
+export const Paragraph = styled.p`
+    text-align: center;
+    font-size: 24px;
+    line-height: 36px;
+    font-weight: 400;
+`;
