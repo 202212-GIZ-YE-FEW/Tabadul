2a065ede8a615a634c9d1d88ada3f49d3173c3df
YassinAbdulrahman <107113173+YassinAbdulrahman@users.noreply.github.com>
2023-03-29 23:02:09 +0300

    feat(categoies): adding Categories Component

---
+++ data/db.js
@@ -0,0 +1,45 @@
+export const CATEGORIES = [
+    "All",
+    "electronics",
+    "jewelery",
+    "men's clothing",
+    "women's clothing",
+];
+
+export const Card = [
+    {
+        id: 1,
+        text: "Buy rice",
+        category: "electronics",
+    },
+    {
+        id: 2,
+        text: "Save a tenner",
+        category: "jewelery",
+    },
+    {
+        id: 3,
+        text: "Build a todo app",
+        category: "men's clothing",
+    },
+    {
+        id: 4,
+        text: "Build todo API",
+        category: "women's clothing",
+    },
+    {
+        id: 5,
+        text: "Get an ISA",
+        category: "electronics",
+    },
+    {
+        id: 6,
+        text: "Cook rice",
+        category: "jewelery",
+    },
+    {
+        id: 7,
+        text: "Tidy house",
+        category: "jewelery",
+    },
+];
--- src/components/Categories/__test__/Categories.test.js
+++ src/components/Categories/__test__/Categories.test.js
@@ -1,8 +1,8 @@
 import renderer from "react-test-renderer";
 
-import Causes from "..";
+import Categories from "..";
 
 it("renders correctly", () => {
-    const tree = renderer.create(<Causes />).toJSON();
+    const tree = renderer.create(<Categories />).toJSON();
     expect(tree).toMatchSnapshot();
 });
