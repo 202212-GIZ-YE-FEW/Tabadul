dc2b16a2b98b1b790ac1066c4db54d95f6320c3b
Sarah Salem <55980263+SarahSalem-hub@users.noreply.github.com>
2023-04-01 03:48:18 +0300

    feat(data): add a local data to simulate cards and filtering by it
    it will be removed lately

---
+++ src/components/ListedItems/data.js
@@ -0,0 +1,72 @@
+export const items = [
+    {
+        id: 0,
+        location: "Sana'a",
+        image: "https://www.greenqueen.com.hk/wp-content/uploads/2021/07/Rental-Fashion-Causes-More-Emissions-Than-Throwing-Clothes-Away.jpg",
+        category: "Clothes",
+        description:
+            "lgsdgsdgsdgls dglsdjglsdjglsd sdlgsdlgldslsdglksdg sdlgsdgsdlglsdlds ",
+        time: "8:00:15",
+        date: "2/30/2000",
+    },
+    {
+        id: 1,
+        location: "Aden",
+        image: "https://www.greenqueen.com.hk/wp-content/uploads/2021/07/Rental-Fashion-Causes-More-Emissions-Than-Throwing-Clothes-Away.jpg",
+        category: "Games",
+        description:
+            "lgsdgsdgsdgls dglsdjglsdjglsd sdlgsdlgldslsdglksdg sdlgsdgsdlglsdlds ",
+        time: "6:00:15",
+        date: "3/05/2023",
+    },
+    {
+        id: 2,
+        location: "Taiz",
+        image: "https://www.greenqueen.com.hk/wp-content/uploads/2021/07/Rental-Fashion-Causes-More-Emissions-Than-Throwing-Clothes-Away.jpg",
+        category: "Books",
+        description:
+            "lgsdgsdgsdgls dglsdjglsdjglsd sdlgsdlgldslsdglksdg sdlgsdgsdlglsdlds ",
+        time: "3:00:15",
+        date: "3/30/2023",
+    },
+    {
+        id: 3,
+        location: "Sana'a",
+        image: "https://www.greenqueen.com.hk/wp-content/uploads/2021/07/Rental-Fashion-Causes-More-Emissions-Than-Throwing-Clothes-Away.jpg",
+        category: "Shoes",
+        description:
+            "lgsdgsdgsdglsdg lsdjglsdjglsd sdlgsdlgldslsdglksdg sdlgsdgsdlglsdlds ",
+        time: "11:00:15",
+        date: "3/30/2022",
+    },
+    {
+        id: 4,
+        location: "Sana'a",
+        image: "https://www.greenqueen.com.hk/wp-content/uploads/2021/07/Rental-Fashion-Causes-More-Emissions-Than-Throwing-Clothes-Away.jpg",
+        category: "Shoes",
+        description:
+            "lgsdgsdgsdglsdg lsdjglsdjglsd sdlgsdlgldslsdglksdg sdlgsdgsdlglsdlds ",
+        time: "12:00:15",
+        date: "3/30/2023",
+    },
+    {
+        id: 5,
+        location: "Sana'a",
+        image: "https://www.greenqueen.com.hk/wp-content/uploads/2021/07/Rental-Fashion-Causes-More-Emissions-Than-Throwing-Clothes-Away.jpg",
+        category: "Shoes",
+        description:
+            "lgsdgsdgsdglsdg lsdjglsdjglsd sdlgsdlgldslsdglksdg sdlgsdgsdlglsdlds ",
+        time: "11:00:15",
+        date: "3/31/2023",
+    },
+    {
+        id: 6,
+        location: "Hadramout",
+        image: "https://www.greenqueen.com.hk/wp-content/uploads/2021/07/Rental-Fashion-Causes-More-Emissions-Than-Throwing-Clothes-Away.jpg",
+        category: "Shoes",
+        description:
+            "lgsdgsdgsdglsdg lsdjglsdjglsd sdlgsdlgldslsdglksdg sdlgsdgsdlglsdlds ",
+        time: "11:00:15",
+        date: "3/30/2023",
+    },
+];
