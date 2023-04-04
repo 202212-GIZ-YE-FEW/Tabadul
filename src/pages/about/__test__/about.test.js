9766cfc0926bf05e9d1bc6d1885afc04c2b59f47
YassinAbdulrahman <107113173+YassinAbdulrahman@users.noreply.github.com>
2023-04-05 02:43:50 +0300

    fix(about): deleteing test from the page

--- src/pages/about/__test__/about.test.js
+++
@@ -1,7 +0,0 @@
-import renderer from "react-test-renderer";
-
-import About from "../index.jsx";
-it("renders correctly", () => {
-    const tree = renderer.create(<About />).toJSON();
-    expect(tree).toMatchSnapshot();
-});
