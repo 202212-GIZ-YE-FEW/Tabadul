8313432630de09f1a3a894a3114c82156f438447
YassinAbdulrahman <107113173+YassinAbdulrahman@users.noreply.github.com>
2023-04-04 03:09:51 +0300

    feat(test): delete test from blog page

--- src/pages/Blogs/__test__/BlogPage.test.js
+++
@@ -1,8 +0,0 @@
-import renderer from "react-test-renderer";
-
-import BlogPage from "..";
-
-it("renders correctly", () => {
-    const tree = renderer.create(<BlogPage />).toJSON();
-    expect(tree).toMatchSnapshot();
-});
