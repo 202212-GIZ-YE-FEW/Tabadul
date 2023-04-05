78ffc43ee6773e161a0cd6dab83d559318063ec9
YassinAbdulrahman <107113173+YassinAbdulrahman@users.noreply.github.com>
2023-04-05 23:22:42 +0300

    fix(fixing): fixing That

--- src/components/Team/index.jsx
+++ src/components/Team/index.jsx
@@ -1,16 +1,9 @@
 import React from "react";
 
-// import { TeamCard, TeamDesc, TeamImg, TeamTitle } from "./Team.styled";
 import { TeamCard, TeamDesc, TeamImg, TeamTitle } from "./Team.styled";
-// import simpleImg from "../../assets/Image/TeamPhoto.svg";
 import mebmerImg from "../../assets/Image/TeamPhoto.svg";
 function Team() {
     return (
-        // <TeamCard>
-        //     <TeamImg src={simpleImg} alt='TeamImg' priority={true} />
-        //     <TeamTitle>Name Surename</TeamTitle>
-        //     <TeamDesc>Description for member</TeamDesc>
-        // </TeamCard>
         <TeamCard>
             <TeamImg src={mebmerImg} alt='member' property={true} />
             <TeamTitle>Surename Y</TeamTitle>
