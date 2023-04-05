9d3cd58b159e7d513f18294dba0361c1f4b6753b
YassinAbdulrahman <107113173+YassinAbdulrahman@users.noreply.github.com>
2023-04-05 22:52:44 +0300

    fix(fixing): fixing

--- src/components/Team/index.jsx
+++ src/components/Team/index.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 
-import { TeamCard, TeamDesc, TeamImg, TeamTitle } from "./Team.styled";
-import simpleImg from "../../assets/Image/TeamPhoto.svg";
+// import { TeamCard, TeamDesc, TeamImg, TeamTitle } from "./Team.styled";
+// import simpleImg from "../../assets/Image/TeamPhoto.svg";
 function Team() {
     return (
-        <TeamCard>
-            <TeamImg src={simpleImg} alt='TeamImg' priority={true} />
-            <TeamTitle>Name Surename</TeamTitle>
-            <TeamDesc>Description for member</TeamDesc>
-        </TeamCard>
+        // <TeamCard>
+        //     <TeamImg src={simpleImg} alt='TeamImg' priority={true} />
+        //     <TeamTitle>Name Surename</TeamTitle>
+        //     <TeamDesc>Description for member</TeamDesc>
+        // </TeamCard>
+        <div>Team</div>
     );
 }
 
