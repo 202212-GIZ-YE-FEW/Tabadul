57cb419be63387cbf92e617b8dbeed9ebf200347
YassinAbdulrahman <107113173+YassinAbdulrahman@users.noreply.github.com>
2023-04-05 17:44:28 +0300

    fix(fixing): fixing Deployment

--- src/components/Team/index.jsx
+++ src/components/Team/index.jsx
@@ -1,14 +1,21 @@
+// import React from "react";
+
+// import { TeamCard, TeamDesc, TeamImg, TeamTitle } from "./Team.styled";
+// function Team({ Surename, Desctitle, MemberImg }) {
+//     return (
+//         <TeamCard>
+//             <TeamImg src={MemberImg} alt='TeamImg' />
+//             <TeamTitle>{Surename}</TeamTitle>
+//             <TeamDesc>{Desctitle}</TeamDesc>
+//         </TeamCard>
+//     );
+// }
+
+// export default Team;
 import React from "react";
 
-import { TeamCard, TeamDesc, TeamImg, TeamTitle } from "./Team.styled";
-function Team({ Surename, Desctitle, MemberImg }) {
-    return (
-        <TeamCard>
-            <TeamImg src={MemberImg} alt='TeamImg' />
-            <TeamTitle>{Surename}</TeamTitle>
-            <TeamDesc>{Desctitle}</TeamDesc>
-        </TeamCard>
-    );
+function Team() {
+    return <div>Team</div>;
 }
 
 export default Team;
