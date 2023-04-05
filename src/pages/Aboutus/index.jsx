7907cf85446282066ae54135ea1cfa9fd10dd6d9
YassinAbdulrahman <107113173+YassinAbdulrahman@users.noreply.github.com>
2023-04-05 23:44:53 +0300

    fix(about page): fixing

--- src/pages/Aboutus/index.jsx
+++ src/pages/Aboutus/index.jsx
@@ -1,11 +1,51 @@
 import React from "react";
 
-import { AboutContainer, AboutSection } from "../../styles/aboutus.styled";
-
+import AboutPhoto from "../../assets/Image/About.svg";
+import {
+    AboutContainer,
+    AboutImg,
+    AboutSection,
+    HeadingContent,
+    HeroSection,
+    ImgContent,
+    ParagrhContent,
+    TeamHeading,
+    TeamMemberContainer,
+    TeamSection,
+    TextContent,
+} from "../../styles/aboutus.styled";
 function Aboutus() {
     return (
         <AboutSection>
-            <AboutContainer></AboutContainer>
+            <AboutContainer>
+                <HeroSection>
+                    <TextContent>
+                        <HeadingContent>Who we are</HeadingContent>
+                        <ParagrhContent>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua. Ut enim ad minim veniam, quis
+                            nostrud exercitation ullamco laboris nisi ut aliquip
+                            ex ea commodo consequat. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat.
+                        </ParagrhContent>
+                    </TextContent>
+                    <ImgContent>
+                        <AboutImg
+                            src={AboutPhoto}
+                            alt='AboutPhoto'
+                            width={896}
+                            height={651}
+                            priority={true}
+                        />
+                    </ImgContent>
+                </HeroSection>
+                <TeamSection>
+                    <TeamHeading>Our Team</TeamHeading>
+                    <TeamMemberContainer>Yes</TeamMemberContainer>
+                </TeamSection>
+            </AboutContainer>
         </AboutSection>
     );
 }
--- src/styles/aboutus.styled.jsx
+++ src/styles/aboutus.styled.jsx
@@ -23,3 +23,91 @@ export const AboutContainer = styled.div`
         width: 1500px;
     }
 `;
+export const HeroSection = styled.div`
+    height: 100vh;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    @media screen and (max-width: 991px) {
+        height: auto;
+        flex-direction: column-reverse;
+    }
+`;
+export const TextContent = styled.div`
+    flex: 1;
+`;
+export const ImgContent = styled.div`
+    flex: 1;
+`;
+export const AboutImg = styled(Image)`
+    max-width: 100%;
+    @media screen and (max-width: 768px) {
+        height: 300px;
+    }
+`;
+export const HeadingContent = styled.h3`
+    font-style: bold;
+    font-weight: 800;
+    font-size: 36px;
+    line-height: 54px;
+    color: #3c4347;
+    @media screen and (max-width: 768px) {
+        font-size: 24px;
+        line-height: 20px;
+        text-align: center;
+    }
+`;
+export const ParagrhContent = styled.p`
+    font-style: normal;
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 36px;
+    width: 720px;
+    color: #3c4347;
+    @media (min-width: 1200px) and (max-width: 1500px) {
+        width: 500px;
+        font-size: 20px;
+        line-height: 30px;
+    }
+    @media (min-width: 768px) and (max-width: 1200px) {
+        width: fit-content;
+    }
+    @media screen and (max-width: 768px) {
+        width: fit-content;
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 30px;
+        text-align: center;
+    }
+`;
+export const TeamSection = styled.div`
+    padding-top: 75px;
+    padding-bottom: 75px;
+    @media screen and (max-width: 768px) {
+        padding-top: 50px;
+        padding-bottom: 50px;
+    }
+`;
+export const TeamHeading = styled.h3`
+    font-style: normal;
+    font-weight: 800;
+    font-size: 36px;
+    line-height: 54px;
+    text-align: center;
+    color: #3c4347;
+    @media screen and (max-width: 768px) {
+        font-size: 24px;
+        line-height: 20px;
+        text-align: center;
+        margin-bottom: 50px;
+        font-weight: 700;
+    }
+`;
+export const TeamMemberContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    gap: 45px;
+`;
