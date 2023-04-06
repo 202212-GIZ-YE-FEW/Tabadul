98af71393336f39cd08e93ccb72bbb38b1bd4fe4
YassinAbdulrahman <107113173+YassinAbdulrahman@users.noreply.github.com>
2023-04-06 18:25:21 +0300

    feat(404 page): create element & Styling

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -2,6 +2,40 @@ import * as React from "react";
 
 import Layout from "@/layout/Layout";
 
+import ErrorPhoto from "../assets/Image/Illu404.svg";
+import ErrorLine from "../assets/Image/Lines.svg";
+import {
+    BackHome,
+    ErorrBox,
+    ErorrContainer,
+    ErrorHeading,
+    ErrorImg,
+    Errorparghrah,
+    ErrorSection,
+    ImgContent,
+    Line,
+    TextContent,
+} from "../styles/error.styled";
 export default function NotFoundPage() {
-    return <Layout>404</Layout>;
+    return (
+        <Layout>
+            <ErrorSection>
+                <ErorrContainer>
+                    <ErorrBox>
+                        <TextContent>
+                            <ErrorHeading>Oooops !</ErrorHeading>
+                            <Errorparghrah>
+                                We can’t seem to find a page you are looking for{" "}
+                                <Line src={ErrorLine} />
+                            </Errorparghrah>
+                            <BackHome href='/'>Back to home</BackHome>
+                        </TextContent>
+                        <ImgContent>
+                            <ErrorImg src={ErrorPhoto} />
+                        </ImgContent>
+                    </ErorrBox>
+                </ErorrContainer>
+            </ErrorSection>
+        </Layout>
+    );
 }
