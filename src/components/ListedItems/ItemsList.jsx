168022ec63cd8ccea9e01cda5fdc52cee834d119
Sarah Salem <55980263+SarahSalem-hub@users.noreply.github.com>
2023-04-07 02:11:58 +0300

    refactor(itemslist): update products-itemslist data names

--- src/components/ListedItems/ItemsList.jsx
+++ src/components/ListedItems/ItemsList.jsx
@@ -11,39 +11,37 @@ import {
 } from "./ListedItems.styled";
 import { Cardinfo, CardParagraph } from "../PopularItems/PopularItems.styled";
 
-const ItemsList = ({ filter }) => {
-    // console.log("filter");
+const ItemsList = ({ filters }) => {
     return (
         <ItemsListDiv>
-            {filter &&
-                filter.map((datum) => {
-                    return (
-                        <ItemCard key={datum.id}>
-                            <CardImage
-                                src={datum.image}
-                                alt='CardImage'
-                                width={258.4}
-                                height={185.44}
+            {filters?.map((item) => {
+                return (
+                    <ItemCard key={item.id}>
+                        <CardImage
+                            src={item.image}
+                            alt='CardImage'
+                            width={258.4}
+                            height={185.44}
+                        />
+                        <Cardinfo>
+                            <ItemIcon
+                                src='/images/Vector.svg'
+                                alt='Icon'
+                                width={17.77}
+                                height={22.61}
                             />
-                            <Cardinfo>
-                                <ItemIcon
-                                    src='/images/Vector.svg'
-                                    alt='Icon'
-                                    width={17.77}
-                                    height={22.61}
-                                />
-                                <ItemLocationTitle>
-                                    {datum.location}
-                                </ItemLocationTitle>
-                            </Cardinfo>
-                            <CardCategory>{datum.category}</CardCategory>
-                            <DateAndTime>
-                                {datum.date} {datum.time}
-                            </DateAndTime>
-                            <CardParagraph>{datum.description}</CardParagraph>
-                        </ItemCard>
-                    );
-                })}
+                            <ItemLocationTitle>
+                                {item.location.name}
+                            </ItemLocationTitle>
+                        </Cardinfo>
+                        <CardCategory>{item.category.name}</CardCategory>
+                        <DateAndTime>
+                            {item.date} {item.time}
+                        </DateAndTime>
+                        <CardParagraph>{item.description}</CardParagraph>
+                    </ItemCard>
+                );
+            })}
         </ItemsListDiv>
     );
 };
