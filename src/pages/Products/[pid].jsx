d7dc12088bd15cffc651125e29fbef10dcc8bd3f
aseelflihan <aseelflihan@gmail.com>
2023-04-14 22:11:27 +0300

    feat(connect single product page with firebase): connect single product
    connect single product page with firebase

--- src/pages/Products/[pid].jsx
+++ src/pages/Products/[pid].jsx
@@ -1,7 +1,47 @@
+import Product from "@/components/ProductPgage/ProductPage";
+
+import { db, fetchItems } from "@/utils/firebase";
+import { doc, getDoc } from "firebase/firestore";
 import React from "react";
 
-const SingleProduct = () => {
-    return <div>SingleProduct</div>;
+const SingleProduct = ({ Item }) => {
+    return (
+        <div>
+            <Product
+                title={Item.title}
+                description={Item.description}
+                images={Item.image}
+                location={Item.location}
+            />
+        </div>
+    );
 };
+
+export async function getStaticPaths() {
+    const Items = await fetchItems();
+
+    const paths = Items.map((Items) => {
+        return {
+            params: {
+                pid: String(Items.id),
+            },
+        };
+    });
+    return {
+        paths,
+        fallback: false, // can also be true or 'blocking'
+    };
+}
+
+export async function getStaticProps({ params }) {
+    const docRef = doc(db, "items", params.pid);
+    const data = await getDoc(docRef);
+    const ItemData = data.data();
+    return {
+        props: {
+            Item: ItemData,
+        },
+    };
+}
 
 export default SingleProduct;
