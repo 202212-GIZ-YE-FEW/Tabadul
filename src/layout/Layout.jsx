384b9a521993f0005ac8630539ed203f6730bd6e
san <sansamaa0@gmail.com>
2023-04-24 03:39:46 +0300

    feat(formating files  with prettier): prettier

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,14 +2,10 @@ import * as React from "react";
 
 import Navbar from "@/components/Navbar/Navbar";
 export default function Layout({ children }) {
-    // Put Header or Footer around the children element
-    // Example
     return (
         <>
             <Navbar />
             {children}
         </>
     );
-
-    // return <>{children}</>;
 }
