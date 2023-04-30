9873984ac8e8a0f7328b0f4fe7da4f563fb2586d
Sarah Salem <55980263+SarahSalem-hub@users.noreply.github.com>
2023-04-30 22:52:00 +0300

    fix(myapp): fix page direction and put Layout inside main tag

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,21 +1,30 @@
 import { Poppins } from "next/font/google";
-import { appWithTranslation } from "next-i18next";
+import { appWithTranslation, i18n } from "next-i18next";
 
 import "@/styles/globals.css";
 import Layout from "@/layout/Layout";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const poppins = Poppins({
     subsets: ["latin"],
-    weight: ["400", "700", "800"],
+    weight: ["400", "700"],
 });
 
 function MyApp({ Component, pageProps }) {
+    const { locale } = useRouter();
+    const dir = locale === "ar" ? "rtl" : "ltr";
+
+    useEffect(() => {
+        document.documentElement.dir = dir;
+    }, [dir]);
+
     return (
-        <Layout>
-            <main className={poppins.className}>
+        <main className={poppins.className}>
+            <Layout>
                 <Component {...pageProps} />
-            </main>
-        </Layout>
+            </Layout>
+        </main>
     );
 }
 
