b37193e1ff1a41b06e06912ce5272e489cb482de
Sarah Salem <55980263+SarahSalem-hub@users.noreply.github.com>
2023-04-30 22:58:21 +0300

    test(navbar): mock useRouter and update the test file

--- src/components/Navbar/__test__/Navbar.test.js
+++ src/components/Navbar/__test__/Navbar.test.js
@@ -2,6 +2,15 @@ import renderer from "react-test-renderer";
 
 import Navbar from "../Navbar";
 
+jest.mock("next/router", () => ({
+    useRouter() {
+        return {
+            pathname: "",
+            // ... whatever else you you call on `router`
+        };
+    },
+}));
+
 it("renders correctly", () => {
     const tree = renderer.create(<Navbar />).toJSON();
     expect(tree).toMatchSnapshot();
