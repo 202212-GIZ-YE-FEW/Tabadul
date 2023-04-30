f644e2fce765f3db1bf2b6c0d377094436b783ce
Sarah Salem <55980263+SarahSalem-hub@users.noreply.github.com>
2023-04-30 22:55:31 +0300

    fix(_document): add locale (language) to Html tag lang property

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -3,12 +3,12 @@ import Document, { Head, Html, Main, NextScript } from "next/document";
 class MyDocument extends Document {
     static async getInitialProps(ctx) {
         const initialProps = await Document.getInitialProps(ctx);
-        return { ...initialProps };
+        return { ...initialProps, locale: ctx?.locale || "en" };
     }
 
     render() {
         return (
-            <Html lang='en'>
+            <Html lang={this.props.locale}>
                 <Head>
                     <link
                         rel='preload'
