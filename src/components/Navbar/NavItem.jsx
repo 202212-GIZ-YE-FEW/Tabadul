1fba090b488ab1a8165866a76b677966f72d85b8
Sarah Salem <55980263+SarahSalem-hub@users.noreply.github.com>
2023-04-30 22:47:53 +0300

    fix(navitem): add router locale to page path

--- src/components/Navbar/NavItem.jsx
+++ src/components/Navbar/NavItem.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 import { Navlink } from "./Nav.styled";
+import { useRouter } from "next/router";
 
 export const NavItem = ({ href, active, text }) => {
+    const router = useRouter();
+
     return (
-        <Navlink href={href} active={active}>
+        <Navlink href={`/${router.locale}${href}`} active={active}>
             <span dangerouslySetInnerHTML={{ __html: text }} />
         </Navlink>
     );
