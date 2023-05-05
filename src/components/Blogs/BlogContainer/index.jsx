5af4b7f46ddc6408e70338e83f0732e581878d03
Sarah Salem <55980263+SarahSalem-hub@users.noreply.github.com>
2023-05-05 16:56:32 +0300

    feat(homepage): add shimmer/loading effect for blogs section

--- src/components/Blogs/BlogContainer/index.jsx
+++ src/components/Blogs/BlogContainer/index.jsx
@@ -6,24 +6,43 @@ import { Header } from "./BlogContainer.styled";
 import SingleBlogCard from "../SingleBlogCard";
 import { BlogCard } from "../SingleBlogCard/SingleBlogCard.styled";
 import { CardLink } from "@/components/ListedItems/ListedItems.styled";
+import { ShimmerDiv, ShimmerItem } from "@/components/Card/Card.styled";
+import { ShimmerPostItem } from "react-shimmer-effects-18";
 
 const BlogContainer = ({ blogs }) => {
     return (
         <>
             <Header>Blogs</Header>
             <BlogCard>
-                {blogs?.slice(0, 4).map((blog) => {
-                    return (
-                        <CardLink key={blog.id} href={`Blogs/${blog.id}`}>
-                            <SingleBlogCard
-                                blogTitle={blog.title}
-                                blog={blog.blog}
-                                blogDate={blog.date}
-                                blogImage={blog.image}
-                            />
-                        </CardLink>
-                    );
-                })}
+                {blogs?.length > 0 ? (
+                    blogs?.slice(0, 4).map((blog) => {
+                        return (
+                            <CardLink key={blog.id} href={`Blogs/${blog.id}`}>
+                                <SingleBlogCard
+                                    blogTitle={blog.title}
+                                    blog={blog.blog}
+                                    blogDate={blog.date}
+                                    blogImage={blog.image}
+                                />
+                            </CardLink>
+                        );
+                    })
+                ) : (
+                    <ShimmerDiv>
+                        <ShimmerItem style={{ width: "280px" }}>
+                            <ShimmerPostItem card title text cta />
+                        </ShimmerItem>
+                        <ShimmerItem style={{ width: "280px" }}>
+                            <ShimmerPostItem card title text cta />
+                        </ShimmerItem>
+                        <ShimmerItem style={{ width: "280px" }}>
+                            <ShimmerPostItem card title text cta />
+                        </ShimmerItem>
+                        <ShimmerItem style={{ width: "280px" }}>
+                            <ShimmerPostItem card title text cta />
+                        </ShimmerItem>
+                    </ShimmerDiv>
+                )}
             </BlogCard>
             <Line />
         </>
