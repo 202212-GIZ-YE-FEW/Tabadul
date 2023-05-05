c5d38118de98aaba064ddd1fc944fa1cb4a84c15
Sarah Salem <55980263+SarahSalem-hub@users.noreply.github.com>
2023-05-05 16:58:00 +0300

    feat(homepage): add shimmer/loading effect for popular products section

--- src/components/PopularItemsSection/index.jsx
+++ src/components/PopularItemsSection/index.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react";
 
 import { Cards, Header, Line } from "../PopularItems/PopularItems.styled";
 import Card from "../Card/Card";
+import { ShimmerDiv, ShimmerItem } from "../Card/Card.styled";
+import { ShimmerPostItem } from "react-shimmer-effects-18";
 
 function PopularItemsSection({ items }) {
-    const [width, setWidth] = useState(1791);
+    const [width, setWidth] = useState(window.innerWidth);
 
     useEffect(() => {
         let cb = function () {
@@ -27,25 +29,43 @@ function PopularItemsSection({ items }) {
             return 4;
         }
     }
+
+    function shimmerCreator(n) {
+        let shimmerArray = [];
+        for (let i = 0; i < n; i++) {
+            shimmerArray.push(
+                <ShimmerItem>
+                    <ShimmerPostItem card title text cta />
+                </ShimmerItem>
+            );
+        }
+        return shimmerArray;
+    }
     return (
         <div>
             <div style={{ position: "relative", top: "-90px" }}>
                 <Header>Popular Items</Header>
                 <Cards>
-                    {items?.slice(0, limitProductsNumber()).map((item) => {
-                        return (
-                            <Card
-                                key={item.id}
-                                productImage={item.image[0].url}
-                                id={item.id}
-                                title={item.title}
-                                location={item.location}
-                                category={item.category}
-                                date={item.date}
-                                description={item.description}
-                            />
-                        );
-                    })}
+                    {items?.length > 0 ? (
+                        items?.slice(0, limitProductsNumber()).map((item) => {
+                            return (
+                                <Card
+                                    key={item.id}
+                                    productImage={item.image[0].url}
+                                    id={item.id}
+                                    title={item.title}
+                                    location={item.location}
+                                    category={item.category}
+                                    date={item.date}
+                                    description={item.description}
+                                />
+                            );
+                        })
+                    ) : (
+                        <ShimmerDiv>
+                            {shimmerCreator(limitProductsNumber())}
+                        </ShimmerDiv>
+                    )}
                 </Cards>
                 <Line />
             </div>
