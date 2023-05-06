d1e357cae826cf8fe775af78387c5dcdb1815a13
Sarah Salem <55980263+SarahSalem-hub@users.noreply.github.com>
2023-05-06 20:57:01 +0300

    fix(addtoitem): fix importing component

--- src/pages/addToItem.jsx
+++ src/pages/addToItem.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { db, fetchCategories, fetchLocations } from "@/utils/firebase";
-import AddItem from "@/components/AddItem";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import AddItem from "@/components/AddItem/Index";
 
 const AddToItem = () => {
     const [categories, setCategories] = useState();
